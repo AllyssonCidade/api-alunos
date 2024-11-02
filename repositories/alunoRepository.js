7e06af8881bfde3a12ab30cbe4e91d8744d6ad68
QuevedoMatheus <matheusquevedobio@gmail.com>
2024-11-02 11:07:55 -0300

    Implementação do metodo CREATE no CRUD

--- repositories/alunoRepository.js
+++ repositories/alunoRepository.js
@@ -3,3 +3,15 @@ const { v4: uuidv4 } = require("uuid");
 let alunos = [];
 
 module.exports = {};
+
+function create({ nome, email, nome_curso }) {
+    const id = uuidv4();
+    const aluno = {
+      id,
+      nome: nome,
+      email: email,
+      nome_curso: nome_curso,
+    };
+    alunos.push(aluno);
+    return { id, nome, email, nome_curso };
+  }
