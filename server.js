575625c6e1b35dae245f710f07f4314df08fcc85
Yan <narederadora@gmail.com>
2024-11-02 14:09:22 -0300

    implemented the findall method on the server

--- server.js
+++ server.js
@@ -16,3 +16,12 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
+app.get("/alunos", (req, res) => {
+  try {
+    const alunos = findAll();
+    res.json(alunos);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao retornar alunos", error });
+  }
+});
